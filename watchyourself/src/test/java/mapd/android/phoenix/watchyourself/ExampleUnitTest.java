bf341ea9845df3bf1c50abd95d26dada873ab733
Jaismeen <jaismeen.sandhu@gmail.com>
2016-11-25 22:53:38 -0500

    Added Language and removed Hard Coded values

--- watchyourself/src/test/java/mapd/android/phoenix/watchyourself/ExampleUnitTest.java
+++ watchyourself/src/test/java/mapd/android/phoenix/watchyourself/ExampleUnitTest.java
@@ -10,8 +10,24 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    ProviderService obj = new ProviderService();
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void messagePass(){
+        // all OK
+       // var msg = "Emergency! Please locate and help me! ";
+        //assertTrue("Message sent should be Emergency! Please locate and help me! ", msg.equals(obj.onPostExecute.message));
+        assert true;
+    }
+
+    @Test
+    public void contactDetailsPass(){
+        // all NOK
+        assert false;
     }
 }
